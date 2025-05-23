de60ce77366be33cde9c6e9c995e20bf696f4197
HolyNitzan <HolyNitzan@users.noreply.github.com>
2025-05-24 02:01:39 +0300

    Update app.php

--- src/Configs/app.php
+++ src/Configs/app.php
@@ -5,7 +5,7 @@ use App\Installer\Database\MySqlSetup;
 return [
     'min_php_version' => '8.4',
 
-    'repository' => 'https://github.com/robertfenyiner/Lat-team-V9',
+    'repository' => 'https://github.com/HDInnovations/UNIT3D',
     
     'database_installers' => [
         /**
